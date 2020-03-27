36c47277a6df6ad5a431878c5f437d69c6bff97b
Eric Lynd <eriklynd@gmail.com>
2020-03-27 11:21:16 +0800

    报错示例

---
+++ src/pages/index/comp.tsx
@@ -0,0 +1,18 @@
+import { Component } from '@tarojs/taro'
+import './index.scss'
+
+export default class Comp extends Component {
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  test() {}
+
+}
